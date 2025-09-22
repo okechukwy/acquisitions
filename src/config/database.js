d2cf88450476c295aab1bb245979b9f07f936392
okechukwu1 <okechukwy@yahoo.com>
2025-09-22 11:44:46 +0100

    IMplement auth

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 
 import { neon} from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 
-import { drizlle} from 'drizzle-orm/neon-http';
+//import { db } from "#config/database.js";
+
 
 const sql = neon(process.env.DATABASE_URL);
-const db = drizlle(sql);
+const db = drizzle(sql);
 
-export default { db, sql};
+export { db, sql};
